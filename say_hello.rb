c0faa749607fa4f0092f57d1457d4adcae5c998f
Toni  Diep <adiep123@gmail.com>
2020-04-06 22:12:38 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,5 @@
 # Build your say_hello method here
-say_hello ("Kent Beck")
 
-def say_hello (name)
-    puts "Hello Kent Beck"
+def say_hello(name = "Ruby Programmer")
+    puts "Hello #{name}!"
 end
